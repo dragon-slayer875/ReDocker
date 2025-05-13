7987eea360efac842c548c327c2b106ae908f16c
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-05-13 17:36:49 +0530

    feat: implement new PID namespace
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- main.go
+++ main.go
@@ -14,23 +14,45 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("found wrong argument")
 	}
 }
 
 func run() {
-	fmt.Println("Running:", os.Args[2:])
+	fmt.Println("Running:", os.Args[2:], "as", os.Getpid())
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 	err := cmd.Run()
 	if err != nil {
 		panic(err)
 	}
+}
+
+func child() {
+	fmt.Println("Running child:", os.Args[2:], "as", os.Getpid())
+
+	syscall.Sethostname([]byte("container"))
+	syscall.Chroot("/")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	syscall.Unmount("proc", 0)
 }
